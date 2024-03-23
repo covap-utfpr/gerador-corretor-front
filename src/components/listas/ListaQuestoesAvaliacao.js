54aff36d42208b24caa6ca1f115a09a9e7d18c42
Helena Rentschler <helrentschler@gmail.com>
2024-03-23 14:53:09 -0300

    at

--- src/components/listas/ListaQuestoesAvaliacao.js
+++ src/components/listas/ListaQuestoesAvaliacao.js
@@ -7,21 +7,13 @@ const ListaQuestoesAvaliacao = () => {
     const { avaliacaoAtual, dispatchAvaliacaoAtual } = useContext(GlobalContext);
     const [tipo, setTipo ] = useState([])
     const [valor, setValor ] = useState([])
-   
-    let somaValorQuestoes = 0;
-    let valoresQuestoes = [];
+    const [ordem, setOrdem ] = useState()
 
     function handleTipo(event) {
         setTipo(event.target.value);
     }  
-    function handleValor(event, indexQuestao){
-        if(somaValorQuestoes + (event.target.value - valoresQuestoes[indexQuestao]) <= avaliacaoAtual.cabecalho.valor){
-            somaValorQuestoes += (event.target.value - valoresQuestoes[indexQuestao]);
-            valoresQuestoes[indexQuestao] = event.target.value;
-        } else{
-            console.log("Valor da avaliação excedido")
-            event.target.value = 0;
-        }
+    function handleValor(event) {
+        setValor(event.target.value);
     }  
 
     useEffect(() => {
@@ -47,31 +39,27 @@ const ListaQuestoesAvaliacao = () => {
             <h2>Questões Adicionadas</h2>
             <ul>
                 <form className="form" onSubmit={(event) => handleSubmit(event)} >
-                    {avaliacaoAtual.questoes && avaliacaoAtual.questoes.map((questao, index) => {
-                        valoresQuestoes.push(0);
-                        return(
-                            <li key={index} value={questao.nome}>
-                                <span>{index}</span>
-                                <span>{questao.nome}</span> 
-                                <div className="campo-form">
-                                    <select name="tipo" id="tipo" onChange={(event) => {handleTipo(event)}}>
-                                        <option key={1} value="vf">V/F</option>
-                                        <option key={2} value="escolha">a,b</option>
-                                        <option key={3} value="descritiva">desc</option>
-                                    </select>
-                                </div>
-                                <div className="campo-form">
-                                    <input 
-                                        type="number"
-                                        name="valor"
-                                        id="valor"
-                                        min="0"
-                                        onChange={(event) => {handleValor(event, index)}}
-                                    />
-                                </div>
-                            </li>
-                        )
-                    })}
+                    {avaliacaoAtual.questoes && avaliacaoAtual.questoes.map((questao, index) => (
+                        <li key={index} value={questao.nome}>
+                            <span>{index}</span>
+                            <span>{questao.nome}</span> 
+                            <div className="campo-form">
+                                <select name="tipo" id="tipo" onChange={(event) => {handleTipo(event)}}>
+                                    <option key={1} value="vf">V/F</option>
+                                    <option key={2} value="escolha">a,b</option>
+                                    <option key={3} value="descritiva">desc</option>
+                                </select>
+                            </div>
+                            <div className="campo-form">
+                                <input 
+                                    type="text"
+                                    name="valor"
+                                    id="valor"
+                                    onChange={(event) => {handleValor(event)}}
+                                />
+                            </div>
+                        </li>
+                    ))}
                     <button type="submit">Editar</button>
                 </form>
             </ul>
